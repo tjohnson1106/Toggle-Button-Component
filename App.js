e708edb3c46a24577ea79224e41ab971901c3f9b
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-04-04 22:16:14 -0400

    created animated button with initial state

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 import ToggleButton from "./src/ToggleButton";
 
 export default class App extends React.Component {
+  state = {
+    value: ""
+  };
+
+  constructor(props) {
+    super(props);
+    this._onStateChange = this._onStateChange.bind(this);
+  }
+
+  _onStateChange(newState) {
+    const value = newState ? "ON" : "OFF";
+    this.setState({ toggleText: value });
+  }
+
   render() {
-    return <ToggleButton />;
+    const { toggleText } = this.state;
+    return (
+      <View style={styles.container}>
+        <ToggleButton onStateChange={this._onStateChange} />
+        <Text style={{ paddingBottom: 60 }}>State: {toggleText}</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
--- src/ToggleButton.js
+++ src/ToggleButton.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
-import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text, Animated } from "react-native";
 
 class ToggleButton extends Component {
   state = {
-    toggle: false
+    toggle: true,
+    animated: new Animated.Value(0)
   };
 
   _onPress() {
+    this.animateButton();
     const newState = !this.state.toggle;
     this.setState({ toggle: newState });
+    this.props.onStateChange && this.props.onStateChange(newState);
+  }
+
+  animateButton() {
+    Animated.timing(this.state.animated, {
+      toValue: 1,
+      duration: 1000
+    }).start;
   }
 
   render() {
-    const { toggle } = this.state;
+    const { toggle, animated } = this.state;
     const textValue = toggle ? "ON" : "OFF";
+    const buttonBg = toggle ? "dodgerblue" : "white";
+    const textColor = toggle ? "white" : "black";
     return (
       <View style={styles.container}>
         <View
@@ -27,19 +39,38 @@ class ToggleButton extends Component {
               margin: 10,
               height: 60,
               flex: 1,
-              backgroundColor: "dodgerblue",
-              justifyContent: "center"
+              backgroundColor: buttonBg,
+              justifyContent: "center",
+              borderColor: "dodgerblue",
+              borderWidth: 2
             }}
           >
-            <Text
+            <Animated.View
               style={{
-                color: "white",
-                textAlign: "center",
-                fontSize: 16
+                flex: 1,
+                backgroundColor: "red",
+                borderRadius: 30,
+                justifyContent: "center",
+                transform: [
+                  {
+                    scale: animated.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [0, 1]
+                    })
+                  }
+                ]
               }}
             >
-              {textValue}
-            </Text>
+              <Text
+                style={{
+                  color: textColor,
+                  textAlign: "center",
+                  fontSize: 16
+                }}
+              >
+                {textValue}
+              </Text>
+            </Animated.View>
           </TouchableOpacity>
         </View>
       </View>
